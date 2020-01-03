64aca06bee7af0bb093daf4d339fd33218ca913a
ELBAGDADI <elbagdadi.abdou@gmail.com>
2020-01-03 17:51:01 +0000

    user system added

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
